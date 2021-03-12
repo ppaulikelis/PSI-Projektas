2261f069cff5019054e103ce414f1a9afa862541
Dzytizz <60034692+Dzytizz@users.noreply.github.com>
2021-03-12 20:14:38 +0200

    Final modifications
    Reworked unit spawning code. Added 3 temporary "units" with temporary movement code for testing. Added UI to see training time left and unit count in queue

--- Age Of War - 4D Edition/Assets/Scripts/UnitSpawner.cs
+++ Age Of War - 4D Edition/Assets/Scripts/UnitSpawner.cs
@@ -1,47 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitSpawner : MonoBehaviour
 {
-    GameObject unitObject;
-    Unit unitData;
+    public Queue<Unit> unitsQueue;
+    Unit currentUnit;
 
-    public bool isOnCooldown = false;
+    public Slider slider;
+    public Toggle[] toggles;
+
+    public bool isTraining = false;
     public float cooldown = 0f;
+
+    private void Start()
+    {
+        unitsQueue = new Queue<Unit>();
+    }
 
     public void Update()
     {
-        if(isOnCooldown)
+       if(!isTraining && unitsQueue.Count > 0)  // if queue has units left: set cooldown to its training time, start training
+        {
+            currentUnit = unitsQueue.Peek();
+            cooldown = (float)currentUnit.trainingTime;
+            isTraining = true;
+        }
+
+       if(isTraining) // if training is in progress: reduce cooldown, set slider value, set UI "boxes" values
         {
             cooldown -= Time.deltaTime;
+
+            slider.value = (float)(currentUnit.trainingTime - cooldown) / (float)currentUnit.trainingTime * slider.maxValue;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < unitsQueue.Count)
+                {
+                    toggles[i].isOn = true;
+                }
+                else
+                {
+                    toggles[i].isOn = false;
+                }
+            }
+
+            if (cooldown <= 0)  // if training is over: create new GameObject, set its data and sprite, remove unit from queue, reset slider and UI "box" value, end training
+            {
+                GameObject newObject = new GameObject("Unit", typeof(UnitControls), typeof(SpriteRenderer));
+                newObject.transform.position = transform.position;
+
+                newObject.GetComponent<UnitControls>().unitData = currentUnit;
+                newObject.GetComponent<SpriteRenderer>().sprite = currentUnit.artwork;
+
+                unitsQueue.Dequeue();
+
+
+                toggles[0].isOn = false;
+                slider.value = 0;
+                isTraining = false;
+            }
         }
-        if(cooldown <= 0)
-        {
-            isOnCooldown = false;
-        }
-        
     }
 
-    public void GenerateUnit(Unit unit)
+    public void GenerateUnit(Unit unit) // code to be run when button is pressed
     {
-        if(!isOnCooldown)
+        if(unitsQueue.Count < 5)
         {
-            unitObject = new GameObject("Unit",typeof(SpriteRenderer),typeof(UnitControls));
-            unitObject.transform.position = transform.position;
-     
-            unitObject.GetComponent<UnitControls>().unit = unit;
-            unitData = unitObject.GetComponent<UnitControls>().unit;
-
-            unitObject.GetComponent<SpriteRenderer>().sprite = unitData.artwork;
-            cooldown = (float)unitData.trainingTime;
-            isOnCooldown = true;
+            unitsQueue.Enqueue(unit);
         }
     }
 }
 
 public class UnitControls : MonoBehaviour
 {
-    public Unit unit;
+    public Unit unitData;
+
+    private void Update()   // temporary code to test movement
+    {
+        gameObject.transform.position += Vector3.right * unitData.movementSpeed * Time.deltaTime;
+
+        if(gameObject.transform.position.x >= 25)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
 
