a383b08a4d88435fa59641e8f56931641e2fb004
Dzytizz <60034692+Dzytizz@users.noreply.github.com>
2021-04-28 21:58:42 +0300

    Code cleanup

--- Age Of War - 4D Edition/Assets/Scripts/EnemyAI.cs
+++ Age Of War - 4D Edition/Assets/Scripts/EnemyAI.cs
@@ -6,8 +6,7 @@ public class EnemyAI : MonoBehaviour
 {
     public int gold;
     public int experience;
-
-    public Unit[] units;
+    float cooldown;
 
     [Tooltip("Chance to spawn enemy every 'spawnCooldown' seconds")]
     [Range(0f, 1f)]
@@ -16,21 +15,26 @@ public class EnemyAI : MonoBehaviour
     public float spawnCooldown;
 
     public GameObject spawner;
+    public Unit[] units;
 
-    void OnValidate()
+    void OnValidate()   // validation of public variables changed in inspector
     {
         spawnCooldown = Mathf.Max(spawnCooldown, 0.1f);
     }
 
     void Start()
     {
+        // starting automatic increase of gold/experience and unit spawning
         StartCoroutine(IncreaseValues(1, 2, 1f));
         StartCoroutine(EnemySpawner(spawnCooldown));
     }
 
     void Update()
     {
-        
+        if(cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
     }
 
     IEnumerator IncreaseValues(int goldIncrease, int expIncrease, float time)
@@ -49,16 +53,17 @@ public class EnemyAI : MonoBehaviour
         {
             yield return new WaitForSeconds(time);
 
-            bool shouldSpawn = Random.Range(0f,1f) < spawnChance;
+            bool shouldSpawn = Random.Range(0f,1f) < spawnChance;   // adding randomness to spawning
 
-            if(shouldSpawn)
+            if(shouldSpawn && cooldown <= 0)
             {
-                int index = Random.Range(0, units.Length);
+                int index = Random.Range(0, units.Length);  // choosing unit
                 if(units[index].cost <= gold)
                 {
-                    gold -= units[index].cost;
                     Unit currentUnit = units[index];
+                    cooldown = currentUnit.trainingTime;
 
+                    // creating enemy unit GameObject and assigning various variables
                     GameObject enemy = new GameObject("Enemy" + currentUnit.name, typeof(UnitControls), typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(Animator));
                     enemy.transform.position = transform.position;
                     enemy.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
@@ -67,6 +72,8 @@ public class EnemyAI : MonoBehaviour
                     UnitControls enemyScript = enemy.GetComponent<UnitControls>();
                     enemyScript.unitData = currentUnit;
                     enemyScript.isEnemy = true;
+
+                    gold -= currentUnit.cost;
                 }
             }
         }
