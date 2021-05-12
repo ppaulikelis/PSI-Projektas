093892fcd853e0393ea0b55653870f7f4b334908
Dzytizz <60034692+Dzytizz@users.noreply.github.com>
2021-05-12 21:15:08 +0300

    Working prototype for enemy towers/turrets

--- Age Of War - 4D Edition/Assets/Scripts/EnemyAI.cs
+++ Age Of War - 4D Edition/Assets/Scripts/EnemyAI.cs
@@ -11,22 +11,39 @@ public class EnemyAI : MonoBehaviour
 
     [Tooltip("Chances for each unit to spawn (e.g. 0.25 = 25%)")]
     public float[] chances= new float[3];
+    [Tooltip("Chance to spawn tower/turret")]
+    public float[] towerChances = new float[2];
     [Tooltip("Chance to spawn enemy every 'spawnCooldown' seconds")]
     [Range(0f, 1f)]
     public float spawnChance;
+    [Tooltip("Chance to spawn enemy tower(0)/turret(1) every 'spawnCooldown' seconds")]
+    [Range(0f, 1f)]
+    public float towerSpawnChance;
     [Tooltip("Cooldown between new chance to spawn enemy in seconds")]
     public float spawnCooldown;
 
+    int towerCount = 0;
+    int turretCount = 0;
+
     public GameObject spawner;
+    public Tower tower;
+    public GameObject towerBase;
+    public GameObject bullet;
+    public Turret turretData;
     public Unit[] units;
 
     void OnValidate()   // validation of public variables changed in inspector
     {
         spawnCooldown = Mathf.Max(spawnCooldown, 0.1f);
-        if(chances.Length != 3)
+        if (chances.Length != 3)
         {
             Debug.LogWarning("Don't change length of chances array in EnemyAI");
             Array.Resize(ref chances, 3);
+        }
+        if(towerChances.Length != 2)
+        {
+            Debug.LogWarning("Don't change length of towerChances array in EnemyAI");
+            Array.Resize(ref towerChances, 2);
         }
     }
 
@@ -61,8 +78,14 @@ public class EnemyAI : MonoBehaviour
         {
             yield return new WaitForSeconds(time);
 
-            bool shouldSpawn = UnityEngine.Random.Range(0f,1f) < spawnChance;   // adding randomness to spawning
+            if (UnityEngine.Random.Range(0f, 1f) < towerSpawnChance)
+            {
+                SpawnTowerOrTurret(towerChances);
+            }
 
+
+            bool shouldSpawn = UnityEngine.Random.Range(0f,1f) < spawnChance;   // adding randomness to spawning
+          
             if(shouldSpawn && cooldown <= 0)
             {
                 int index = GetIndexFromChances(chances, 0);    // generate random index according to chances array
@@ -94,5 +117,58 @@ public class EnemyAI : MonoBehaviour
         else if (current <= chances[0] + chances[1]) return 1;
         else if (current <= chances[0] + chances[1] + chances[2]) return 2;
         else return defaultIndex;
+    }
+
+    private void SpawnTowerOrTurret(float[] chances)
+    {
+        float current = UnityEngine.Random.Range(0f, 1f);
+        if (current <= chances[0])  // build tower
+        {
+            if(gold >= tower.cost[towerCount] && towerCount <3)
+            {
+                gold -= tower.cost[towerCount];
+                GameObject newTower = new GameObject("Enemy Tower" + towerCount, typeof(SpriteRenderer));     // tower creation
+                newTower.GetComponent<SpriteRenderer>().sprite = tower.artwork;
+                newTower.transform.position = new Vector3(towerBase.transform.position.x, tower.yPlacement[towerCount], 0);
+                newTower.transform.SetParent(towerBase.gameObject.transform);
+                towerCount++;
+            }
+         
+        }
+        else if(current <= chances[0] + chances[1]) // build turret (if tower exists)
+        {
+            if(gold >= turretData.price && towerBase.transform.childCount > turretCount && turretCount < 3)
+            {
+                gold -= turretData.price;
+                Transform turretSpot = towerBase.transform.GetChild(turretCount);
+
+                GameObject turret = new GameObject("Button", typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(TurretController));  // turret creation
+                turret.transform.SetParent(towerBase.transform.GetChild(turretCount));
+                TurretController script = turret.GetComponent<TurretController>();  // setting up TurretController script using turretData
+                script.turretData = turretData;
+                script.UpdateVariables();
+                Transform newShootpoint = new GameObject("Shootpoint").transform;
+                newShootpoint.SetParent(turret.transform);
+                script.shootPoint = newShootpoint;
+                script.shootPoint.localPosition = new Vector2(-turretData.shootpointOffsetX, 0);
+                script.targetTag = "Friendly";
+                script.bullet = bullet;
+
+                turret.transform.localPosition = Vector2.zero;  // moving turret to middle of tower block
+
+                SpriteRenderer rend = turret.GetComponent<SpriteRenderer>();    // setting up sprite (half transparent, sorting order)
+                rend.sprite = turretData.artwork;
+                rend.sortingOrder = towerCount + 1;
+                rend.flipX = true;
+                turret.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
+                turret.GetComponent<BoxCollider2D>().isTrigger = true;
+
+                turretCount++;
+            }
+        }
+        else
+        {
+            return;
+        }
     }
 }
